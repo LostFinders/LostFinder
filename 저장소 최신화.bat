35c77d2c1f522c413ce8b47d23396f06fc9e172b
totoqqqqDT <totoqqqqqq@gmail.com>
2022-11-01 10:14:14 +0900

    commit keyword(ex : 0.2.1 - edit log)

---
+++ 저장소 최신화.bat
@@ -0,0 +1,6 @@
+@echo
+==================================
+git remote add leadrepo https://github.com/LostFinders/LostFinder
+git fetch leadrepo
+git merge leadrepo/master
+git push myforkrepo master
